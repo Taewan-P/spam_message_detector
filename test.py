ea6c2f546cc0d8701df2b4da2a9d44fc028dbb27
Taewan-P <swimingtw@gmail.com>
2020-02-05 22:03:09 +0900

    Update test code based on README.md
    To Be Continued...

--- test.py
+++ test.py
@@ -1,7 +1,53 @@
 import csv
+from math import log
+from collections import defaultdict
+import re
+import math
+import numpy as np
+import random
+import pandas as pd
+import matplotlib.pyplot as plt
+from krwordrank.word import KRWordRank
+
+
+def count_keywords(word): # returns [spam_count, ham_count]
+  result = [0,0]
+  for i in total: # i is ['message content', 'ham/spam']
+    if word in i[0]:
+      if i[1] == "spam":
+        result[0] += 1
+      else: # ham
+        result[1] += 1
+  return result
+
+
 with open('messages.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile)
-    for row in spamreader: # row is a list ['message content', 'ham/spam']
-        a = row[0].replace('\n', ' ') # Delete new lines
-        row[0] = a
-        print(row)
+  spamreader = csv.reader(csvfile)
+  analysis = []
+  total = []
+  for row in spamreader: # row is a list ['message content', 'ham/spam']
+    a = row[0].replace('\n', ' ') # Delete new lines
+    row[0] = a # Only content
+    total.append(row) # Whole data
+    if row[1] == 'spam':
+      analysis.append(a) # Analysis for only spam messages
+
+  wordrank_extractor = KRWordRank(
+    min_count = 5,
+    max_length = 10,
+    verbose = True
+  )
+  beta = 0.85
+  max_iter = 10
+
+  keywords, rank, graph = wordrank_extractor.extract(analysis, beta, max_iter)
+  spam_keywords = list(keywords)
+  top_spam_keywords = spam_keywords[1:6] # This keyword list should be changed depending on what you wish to train.
+
+  counted_keywords = {}
+  for k in top_spam_keywords:
+    newkey = {k: count_keywords(k)}
+    counted_keywords.update(newkey)
+  print(counted_keywords)
+  
+  
